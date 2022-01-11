48ddb98f82998f8186093d94cf60ebbe56d941bb
QR <1826255833@qq.com>
2022-01-11 23:07:00 +0800

    shiroConfig 修改

--- core/src/main/java/zhku/graduation/core/config/shiro/ShiroConfig.java
+++ core/src/main/java/zhku/graduation/core/config/shiro/ShiroConfig.java
@@ -5,8 +5,6 @@ import org.apache.shiro.mgt.DefaultSubjectDAO;
 import org.apache.shiro.mgt.SubjectFactory;
 import org.apache.shiro.realm.Realm;
 import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
-import org.apache.shiro.web.filter.authc.AnonymousFilter;
-import org.apache.shiro.web.filter.authc.LogoutFilter;
 import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,12 +16,11 @@ import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
-//springBoot整合jwt实现认证有三个不一样的地方，对应下面abc
 @Configuration
 public class ShiroConfig {
 
     /*
-     * a. 告诉shiro不要使用默认的DefaultSubject创建对象，因为不能创建Session
+     * 不创建 Session
      * */
     @Bean
     public SubjectFactory subjectFactory() {
@@ -39,9 +36,6 @@ public class ShiroConfig {
     public DefaultWebSecurityManager securityManager() {
         DefaultWebSecurityManager securityManager = new DefaultWebSecurityManager();
         securityManager.setRealm(realm());
-        /*
-         * b
-         * */
         // 关闭 ShiroDAO 功能
         DefaultSubjectDAO subjectDAO = new DefaultSubjectDAO();
         DefaultSessionStorageEvaluator defaultSessionStorageEvaluator = new DefaultSessionStorageEvaluator();
@@ -58,24 +52,15 @@ public class ShiroConfig {
     public ShiroFilterFactoryBean shiroFilterFactoryBean() {
         ShiroFilterFactoryBean shiroFilter = new ShiroFilterFactoryBean();
         shiroFilter.setSecurityManager(securityManager());
-        shiroFilter.setLoginUrl("/unauthenticated");
-        shiroFilter.setUnauthorizedUrl("/unauthorized");
-        /*
-         * c. 添加jwt过滤器，并在下面注册
-         * 也就是将jwtFilter注册到shiro的Filter中
-         * 指定除了login和logout之外的请求都先经过jwtFilter
-         * */
         Map<String, Filter> filterMap = new HashMap<>();
-        //这个地方其实另外两个filter可以不设置，默认就是
-        filterMap.put("anon", new AnonymousFilter());
+        // 注册 jwt 拦截器
         filterMap.put("jwt", new JwtFilter());
-        filterMap.put("logout", new LogoutFilter());
         shiroFilter.setFilters(filterMap);
-
         // 拦截器
         Map<String, String> filterRuleMap = new LinkedHashMap<>();
         filterRuleMap.put("/login", "anon");
         filterRuleMap.put("/logout", "logout");
+        // 所有的接口都要走 jwt 拦截规则
         filterRuleMap.put("/**", "jwt");
         shiroFilter.setFilterChainDefinitionMap(filterRuleMap);
 
