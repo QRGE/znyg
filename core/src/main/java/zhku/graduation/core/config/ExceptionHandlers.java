3addf981997161af6407a19507f6e0cc9a1db04c
QR <1826255833@qq.com>
2022-01-15 09:44:14 +0800

    update

--- core/src/main/java/zhku/graduation/core/config/ExceptionHandlers.java
+++ core/src/main/java/zhku/graduation/core/config/ExceptionHandlers.java
@@ -17,10 +17,23 @@ import zhku.graduation.basic.vo.Result;
  */
 @RestControllerAdvice
 public class ExceptionHandlers extends BaseController {
-
-    @ExceptionHandler(Exception.class)
-    public Result<?> handleException() {
-        return error(HttpStatus.ERROR);
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    public Result<?> HttpRequestMethodNotSupportedException(HttpRequestMethodNotSupportedException e){
+        StringBuilder sb = new StringBuilder();
+        sb.append("不支持 ");
+        sb.append(e.getMethod());
+        sb.append(" 请求方法，");
+        sb.append("支持方法: ");
+        String [] methods = e.getSupportedMethods();
+        assert methods != null;
+        for(String str:methods){
+            sb.append(str);
+            sb.append("、");
+        }
+        String result = sb.toString();
+        // 把最后一个顿号去掉
+        result = result.substring(0, result.length()-1);
+        return Result.error(10005, result);
     }
 
     @ExceptionHandler(InValidTokenException.class)
@@ -33,22 +46,10 @@ public class ExceptionHandlers extends BaseController {
         return error(HttpStatus.NO_AUTH_ERROR);
     }
 
-    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
-    public Result<?> HttpRequestMethodNotSupportedException(HttpRequestMethodNotSupportedException e){
-        StringBuilder sb = new StringBuilder();
-        sb.append("不支持 ");
-        sb.append(e.getMethod());
-        sb.append(" 请求方法，");
-        sb.append("支持: ");
-        String [] methods = e.getSupportedMethods();
-        assert methods != null;
-        for(String str:methods){
-            sb.append(str);
-            sb.append("、");
-        }
-        String result = sb.toString();
-        // 把最后一个顿号去掉
-        result = result.substring(0, result.length()-1);
-        return Result.error(10005, result);
+    @ExceptionHandler(Exception.class)
+    public Result<?> handleException() {
+        return error(HttpStatus.ERROR);
     }
+
+
 }
