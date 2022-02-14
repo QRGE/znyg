767adb4c8066b0f261b36f876db9b3af392ab51c
QR <1826255833@qq.com>
2022-02-14 10:30:28 +0800

    开始和删除定时任务

--- core/src/main/java/zhku/graduation/core/modules/quartz/controller/QuartzController.java
+++ core/src/main/java/zhku/graduation/core/modules/quartz/controller/QuartzController.java
@@ -3,9 +3,7 @@ package zhku.graduation.core.modules.quartz.controller;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
-import org.quartz.JobKey;
-import org.quartz.Scheduler;
-import org.quartz.TriggerKey;
+import org.quartz.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import zhku.graduation.basic.constant.Constant;
 import zhku.graduation.basic.controller.BaseController;
 import zhku.graduation.basic.vo.Result;
+import zhku.graduation.core.modules.quartz.job.MockRecordJob;
 
 /**
  * @author qr
@@ -37,7 +36,28 @@ public class QuartzController extends BaseController {
         } catch (Exception e) {
             log.error(e.getMessage(), e);
         }
-        log.info("删除任务成功");
+        log.info("删除定时任务：{}", Constant.JOB_DETAIL);
+        return Result.OK();
+    }
+
+    @ApiOperation("开始执行历史mock数据任务")
+    @GetMapping("mock/start")
+    public Result<?> startMock() throws SchedulerException {
+        JobDetail jobDetail = JobBuilder.newJob(MockRecordJob.class)
+                .withIdentity(Constant.JOB_DETAIL)
+                .storeDurably()
+                .build();
+        CronScheduleBuilder scheduleBuilder = CronScheduleBuilder.cronSchedule("0/5 * * * * ? *");
+        // 创建任务触发器
+        Trigger trigger = TriggerBuilder.newTrigger()
+                .forJob(jobDetail)
+                .withIdentity(Constant.TRIGGER)
+                .withSchedule(scheduleBuilder)
+                .startNow() //立即執行一次任務
+                .build();
+        // 手动将触发器与任务绑定到调度器内
+        scheduler.scheduleJob(jobDetail, trigger);
+        log.info("开始执行定时任务：{}", Constant.JOB_DETAIL);
         return Result.OK();
     }
 }
