0a0b828b1cf867d6936a4f10ab632ab78a76453f
QR <1826255833@qq.com>
2022-02-25 13:48:27 +0800

    更新跨域配置

--- core/src/main/java/zhku/graduation/core/config/filter/CorsFilter.java
+++ core/src/main/java/zhku/graduation/core/config/filter/CorsFilter.java
@@ -1,8 +1,8 @@
 package zhku.graduation.core.config.filter;
 
-import javax.servlet.FilterConfig;
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 /**
@@ -10,7 +10,7 @@ import java.io.IOException;
  * @date 2022/2/25 10:58
  */
 @WebFilter(value = "/*", filterName = "global-filter")
-public class GlobalFilter implements Filter {
+public class CorsFilter implements Filter {
 
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
@@ -20,7 +20,12 @@ public class GlobalFilter implements Filter {
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         // 测试过滤器
-        System.out.println("Global Filter");
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        response.setHeader("Access-Control-Allow-Origin", "http://localhost:10002");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+        response.setHeader("Access-Control-Allow-Headers", "x-requested-with,content-type");
         // 用过滤器链,去进入到下一个过滤器
         filterChain.doFilter(servletRequest, servletResponse);
     }
