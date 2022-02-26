c03983b01680de484fc30a013e220fbc63a2c470
QR <1826255833@qq.com>
2022-02-26 17:01:07 +0800

    update

--- core/src/main/java/zhku/graduation/core/modules/record/mapper/MonitorRecordMapper.java
+++ core/src/main/java/zhku/graduation/core/modules/record/mapper/MonitorRecordMapper.java
@@ -1,6 +1,7 @@
 package zhku.graduation.core.modules.record.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import zhku.graduation.core.modules.record.entity.po.MonitorRecord;
 
@@ -14,6 +15,7 @@ import java.util.Date;
  * @author QR
  * @since 2022-02-14
  */
+@Mapper
 public interface MonitorRecordMapper extends BaseMapper<MonitorRecord> {
 
     int countMonitorRecords(@Param("nodeId") Integer nodeId, @Param("startTime") Date startTime, @Param("endTime") Date endTime);
