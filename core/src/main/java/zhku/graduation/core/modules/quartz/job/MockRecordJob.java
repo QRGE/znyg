2e69985894e6cb35c59b87f7f357aaee72f03b60
QR <1826255833@qq.com>
2022-04-05 14:29:54 +0800

    mock 任务修改

--- core/src/main/java/zhku/graduation/core/modules/quartz/job/MockRecordJob.java
+++ core/src/main/java/zhku/graduation/core/modules/quartz/job/MockRecordJob.java
@@ -6,6 +6,8 @@ import org.quartz.JobExecutionContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.quartz.QuartzJobBean;
 import org.springframework.stereotype.Component;
+import zhku.graduation.core.modules.limit.entity.bean.TemperatureLimitDetail;
+import zhku.graduation.core.modules.limit.service.ITemperatureLimitService;
 import zhku.graduation.core.modules.node.service.INodeService;
 import zhku.graduation.core.modules.record.entity.po.MonitorRecord;
 import zhku.graduation.core.modules.record.service.IMonitorRecordService;
@@ -28,6 +30,8 @@ public class MockRecordJob extends QuartzJobBean {
     private IMonitorRecordService monitorRecordService;
     @Autowired
     private INodeService nodeService;
+    @Autowired
+    private ITemperatureLimitService temperatureLimitService;
 
     @Override
     protected void executeInternal(JobExecutionContext jobExecutionContext) {
@@ -40,6 +44,10 @@ public class MockRecordJob extends QuartzJobBean {
         monitorRecord.setTemperature(BigDecimal.valueOf(RandomUtil.randomDouble(9.1, 38.4)).setScale(1, RoundingMode.HALF_UP).doubleValue());
         // 记录日期
         monitorRecord.setRecordTime(new Date());
+        // 记录温度上下限
+        TemperatureLimitDetail temperatureLimit = temperatureLimitService.getTemperatureLimit(nodeId);
+        monitorRecord.setTemperatureLowerLimit(temperatureLimit.getTemperatureLowerLimit());
+        monitorRecord.setTemperatureUpperLimit(temperatureLimit.getTemperatureUpperLimit());
         // 保存到数据库
         monitorRecordService.save(monitorRecord);
     }
