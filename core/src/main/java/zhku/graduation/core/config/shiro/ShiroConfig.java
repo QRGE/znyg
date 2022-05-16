6f0322404cf69d83fd88772ba4f823d0b208f416
QR <1826255833@qq.com>
2022-05-16 14:44:40 +0800

    发送工具类接口

--- core/src/main/java/zhku/graduation/core/config/shiro/ShiroConfig.java
+++ core/src/main/java/zhku/graduation/core/config/shiro/ShiroConfig.java
@@ -61,12 +61,12 @@ public class ShiroConfig {
         // 先只放行登陆和推出
         filterUrls.put("/user/login", "anon");
         filterUrls.put("/user/logout", "anon");
+        filterUrls.put("/tool/**", "anon"); // 工具类, 例如发送邮箱验证码
 //        filterUrls.put("/user/**", "anon"); // 用户相关
 //        filterUrls.put("/node/**", "anon"); // 鱼缸节点
 //        filterUrls.put("/quartz/**","anon"); // 定时任务
 //        filterUrls.put("/limit/**", "anon"); // 发送命令
 //        filterUrls.put("/command/**", "anon"); // 控制命令
-//        filterUrls.put("/tool/**", "anon"); // 工具类
 //        filterUrls.put("/record/**", "anon"); // 历史记录
         // 所有的接口都要走 jwt 拦截规则
         filterUrls.put("/**", "jwt");
